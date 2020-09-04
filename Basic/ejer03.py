869ddb72a2d09ba862dc7b1732c44fd75419bb5e
g5sh <eagomez2387@gmail.com>
2020-09-04 00:28:48 -0500

    [ADD] PRACTICE: Only practice

---
+++ Basic/ejer03.py
@@ -0,0 +1,10 @@
+# comentarios y docstring
+edad = 23  # edad minima de la persona
+
+
+def meses(years):
+    """Funcion para calcular la edad en meses"""
+    return years * 12
+
+
+print(meses(edad))
